c60bd12716d6d91f33a54ed4bc45c59db4c126fc
Cineplex <majorkittipoj@gmail.com>
2026-05-11 13:29:13 +0700

    นำส่วนของ Godot ออก

--- frontend/android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,23 +1,7 @@
 package com.example.flutter_application_1
 
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    private val CHANNEL = "com.example.flutter_application_1/godot"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "launchGodot") {
-                val intent = Intent(this, GameActivity::class.java)
-                startActivity(intent)
-                result.success(null)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
+    
 }
